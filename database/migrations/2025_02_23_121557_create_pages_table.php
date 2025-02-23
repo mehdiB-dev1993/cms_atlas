f6e6c5b09e40b5d5e4168bef8efb284d5175eaf5
mehdi bakhshi <mehdi.1993.dev@gmail.com>
2025-02-23 04:27:25 -0800

    create_migration_for_menu_and_page

---
+++ database/migrations/2025_02_23_121557_create_pages_table.php
@@ -0,0 +1,43 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('pages', function (Blueprint $table) {
+            $table->id();
+            $table->foreignId('menu_id')->index()->constrained('menus')->onDelete('cascade');
+            $table->foreignId('gallery_id')->index()->constrained('gallery')->onDelete('cascade');
+            $table->string('title');
+            $table->string('title_in_menu');
+            $table->text('text')->nullable();
+            $table->text('full_text')->nullable();
+            $table->text('description')->nullable();
+            $table->string('keywords')->nullable();
+            $table->string('source');
+            $table->string('header_image');
+            $table->string('thumb')->nullable();
+            $table->string('icon')->nullable();
+            $table->string('date')->index();
+            $table->integer('order')->index();
+            $table->integer('status')->default(1)->index();
+            $table->softDeletes();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('pages');
+    }
+};
---
+++ database/migrations/2025_02_23_121614_create_menus_table.php
@@ -0,0 +1,39 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('menus', function (Blueprint $table) {
+            $table->id()->index();
+            $table->foreignId('parent_id')->index();
+            $table->string('title');
+            $table->text('description');
+            $table->text('text');
+            $table->text('full_text');
+            $table->string('keywords');
+            $table->string('thumb');
+            $table->string('icon');
+            $table->string('header_image');
+            $table->integer('order')->index();
+            $table->integer('status')->default(0)->index();
+            $table->softDeletes();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('menus');
+    }
+};
