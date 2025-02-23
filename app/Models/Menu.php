2a5b0bd2446eb93226a7b3365427e20710fedd99
mehdi bakhshi <mehdi.1993.dev@gmail.com>
2025-02-23 04:40:46 -0800

    create model and controller for menu and page and create relation 1-n

---
+++ app/Models/Menu.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Menu extends Model
+{
+    protected $fillable = [
+        'parent_id',
+        'title',
+        'description',
+        'text',
+        'full_text',
+        'keywords',
+        'thumb',
+        'icon',
+        'header_image',
+        'order',
+        'status'
+    ];
+
+
+    public function page(): \Illuminate\Database\Eloquent\Relations\BelongsTo
+    {
+        return $this->belongsTo(Page::class);
+    }
+}
---
+++ app/Models/Page.php
@@ -0,0 +1,34 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Page extends Model
+{
+    protected $fillable =
+        [
+            'menu_id',
+            'gallery_id',
+            'title',
+            'title_in_menu',
+            'text',
+            'full_text',
+            'description',
+            'keywords',
+            'source',
+            'header_image',
+            'thumb',
+            'icon',
+            'date',
+            'order',
+            'status '
+        ];
+
+
+        public function menus(): \Illuminate\Database\Eloquent\Relations\HasMany
+        {
+            return $this->hasMany(Menu::class);
+        }
+
+}
